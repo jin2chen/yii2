cfd77ef72a00b9eadc86e551b4d04bd44234e520
Jin.Chen <jin.chen@starlight-sms.com>
2016-04-06 16:48:29 +0800

    Change client status code.

--- src/rest/ClientException.php
+++ src/rest/ClientException.php
@@ -19,6 +19,6 @@ class ClientException extends HttpException
      */
     public function __construct($message = null, $code = 0, \Exception $previous = null)
     {
-        parent::__construct(400, $message, $code, $previous);
+        parent::__construct(449, $message, $code, $previous);
     }
 }
